5d2a37083bd95fe4ef517dc3175f52432f9bc23b
BigMommaPr <egoregormart09@gmail.com>
2023-05-28 16:03:20 +0500

    Add jump animation and end of RunAndGun level

--- Assets/Scripts/Boss Infinity/LevelControllers/SpawnSystemSt2.cs
+++ Assets/Scripts/Boss Infinity/LevelControllers/SpawnSystemSt2.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class SpawnSystemSt2 : SpawnSystem
 {
-    // protected override Enemies SelectEnemy(int spawnerNumber)
-    // {
-    //     return spawnerNumber < 6 ? (Enemies)Random.Range(0, 2) : Enemies.Psi;
-    // }
+    protected override Enemies SelectEnemy(int spawnerNumber)
+    {
+        return spawnerNumber < 6 ? (Enemies)Random.Range(0, 2) : Enemies.Psi;
+    }
 }
